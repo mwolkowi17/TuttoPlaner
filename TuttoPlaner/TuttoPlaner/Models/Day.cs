dab3eeddcc3e234909a048432724f176dacc5363
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-01-07 14:05:24 +0100

    Basic model set

---
+++ TuttoPlaner/TuttoPlaner/Models/Day.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TuttoPlaner.Models
+{
+    class Day
+    {
+        public int DayId { get; set; }
+        public int DayNumber { get; set; }
+        public int DayMonth { get; set; }
+        public string DayPlans { get; set; }
+    }
+}
