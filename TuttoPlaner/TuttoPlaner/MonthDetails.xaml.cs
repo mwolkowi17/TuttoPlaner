0c94a89009c123e55628731788854992db47dd12
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-01-08 17:57:15 +0100

    MonthDetails added

---
+++ TuttoPlaner/TuttoPlaner/MonthDetails.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TuttoPlaner
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MonthDetails : ContentPage
+    {
+        public MonthDetails()
+        {
+            InitializeComponent();
+        }
+    }
+}
