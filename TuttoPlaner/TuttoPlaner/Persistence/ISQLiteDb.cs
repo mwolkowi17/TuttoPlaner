98a0ed240c2c4825762e4dbdbc4a908c33d9fd7c
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-01-10 20:18:57 +0100

    seting db start

---
+++ TuttoPlaner/TuttoPlaner/Persistence/ISQLiteDb.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TuttoPlaner
+{
+   
+        public interface ISQLiteDb
+        {
+            SQLiteAsyncConnection GetConnection();
+        }
+    
+}
